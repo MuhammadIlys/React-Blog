221ca48956d18ea94a44c2c8fee8c96061cdabb1
MuhammadIlys <saylikhan97@gmail.com>
2023-09-27 18:45:42 +0500

    Initial React Blog Commit

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import './App.css';
+import Navbar from './Components/Navbar';
+import Header from './Components/Myheader';
+import Footer from './Components/Myfooter';
+import Home from './Components/Home';
+import About from './Components/About';
+import SamplePost from './Components/SamplePost';
+import Contact from './Components/Contact';
+import Nopage from './Components/Nopage';
+import { Routes } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { useEffect, useState } from 'react';
+
+
+function App() {
+
+  const [auth,setAuth] = useState(0);
+  const loginhandler = () => {
+   if(auth==1){
+    alert('auth one' + auth);
+    setAuth(0);
+   }else{
+    alert('auth zero' + auth);
+    setAuth(1);
+   }
+  };
+  // useEffect(() => {
+  //   alert('Effect auth' + auth); // This will alert the updated (new) value of auth
+  // }, [auth]);
+
+  return (
+    <>
+      <div className="App">
+        <Router>
+          <Navbar login={loginhandler} state={auth} />
+          {/* <Header /> */}
+          <Routes>
+            <Route path="/" element={<Home />} ></Route>
+            <Route path="/about" element={<About />}></Route>
+            <Route path="/contact" element={<Contact />}></Route>
+            <Route path="/sample" element={<SamplePost />}></Route>
+            <Route path="*" element={<Nopage />}></Route>
+          </Routes>
+        </Router>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Nopage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Nopage() {
+  return (
+    <header class="masthead" style={{ backgroundImage: 'url("assets/img/home-bg.jpg")' }}>
+      <div class="container position-relative px-4 px-lg-5">
+        <div class="row gx-4 gx-lg-5 justify-content-center">
+          <div class="col-md-10 col-lg-8 col-xl-7">
+            <div class="site-heading">
+              <h1>No Page</h1>
+              <span class="subheading">This Page Could Not Be Found</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  )
+}
